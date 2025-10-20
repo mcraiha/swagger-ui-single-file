500b4f6338a042490acb86c43ff2dac956b015ff
Kaarlo Räihä <mcraiha@users.noreply.github.com>
2025-10-20 20:54:19 +0300

    More WIP on static web
    Almost there

--- src/ts/staticweb/main.ts
+++ src/ts/staticweb/main.ts
@@ -4,6 +4,11 @@ const filelink = document.getElementById("filelink")!;
 const urleditSection = document.getElementById("urledit")!;
 const fileeditSection = document.getElementById("fileedit")!;
 
+const urlgenerateButton = document.getElementById("urlgenerate")!;
+const filegenerateButton = document.getElementById("filegenerate")!;
+
+const htmlOutputFrame = document.getElementById("htmlOutputFrame")! as HTMLIFrameElement;
+
 urllink.addEventListener("click", function() {
     selectUrlEdit();
 });
@@ -11,6 +16,14 @@ urllink.addEventListener("click", function() {
 filelink.addEventListener("click", function() {
     selectFileEdit();
 });
+
+urlgenerateButton.addEventListener("click", async function() {
+    let templateText = await checkDownloadStore();
+    htmlOutputFrame.srcdoc = templateText;
+    htmlOutputFrame.hidden = false;
+});
+
+const templateFilePrefix: string = "index-5-29";
 
 function selectUrlEdit() {
     showUrlEditSection();
@@ -36,4 +49,30 @@ function showFileEditSection() {
 
 function hideFileEditSection() {
     fileeditSection.hidden = true;
+}
+
+const localStorageTemplateKey: string = "template";
+
+async function checkDownloadStore(): Promise<string> {
+    let returnString: string = "";
+    if (localStorage.getItem(localStorageTemplateKey) === null) {
+        // Fetch the template
+        try
+        {
+            const response = await fetch(`${templateFilePrefix}.html`);
+            const text = await response.text();
+            if (text.startsWith("<!--")) {
+                localStorage.setItem(localStorageTemplateKey, text);
+                returnString = text;
+            }
+        }
+        catch (error) {
+            console.error(error);
+        }
+    }
+    else {
+        returnString = localStorage.getItem(localStorageTemplateKey)!;
+    }
+
+    return returnString;
 }
