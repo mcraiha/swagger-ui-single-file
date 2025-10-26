b8ece466c99a946ca8c7fc8437aba80a584728af
Kaarlo Räihä <mcraiha@users.noreply.github.com>
2025-10-26 15:02:48 +0200

    First working web version
    Not perfect

--- src/ts/staticweb/main.ts
+++ src/ts/staticweb/main.ts
@@ -7,7 +7,9 @@ const fileeditSection = document.getElementById("fileedit")!;
 const urlinput = document.getElementById("urlinput")! as HTMLInputElement;
 
 const urlgenerateButton = document.getElementById("urlgenerate")!;
-const filegenerateButton = document.getElementById("filegenerate")!;
+const inputFile = document.getElementById("inputfile")! as HTMLInputElement;
+
+const downloadButton = document.getElementById("downloadbutton")!;
 
 const htmlOutputHeader = document.getElementById("htmlOutputHeader")!;
 const htmlOutputFrame = document.getElementById("htmlOutputFrame")! as HTMLIFrameElement;
@@ -20,13 +22,43 @@ filelink.addEventListener("click", function() {
     selectFileEdit();
 });
 
+downloadButton.addEventListener("click", function() {
+    handleDownload();
+});
+
 urlgenerateButton.addEventListener("click", async function() {
     let templateText = await checkDownloadStore();
     templateText = templateText.replace(`https://petstore.swagger.io/v2/swagger.json`, urlinput.value);
+
     htmlOutputFrame.srcdoc = templateText;
     htmlOutputFrame.hidden = false;
     htmlOutputHeader.hidden = false;
+    downloadButton.hidden = false;
 });
+
+inputFile.addEventListener("change", async function() {
+    // Check input file first
+    const file: File = this.files?.item(0)!;
+
+    try
+    {
+        const possibleJsonInput: string = await file.text();
+        JSON.parse(possibleJsonInput);
+
+        let templateText = await checkDownloadStore();
+        templateText = templateText.replace(`url: "https://petstore.swagger.io/v2/swagger.json"`, `spec: ${possibleJsonInput}`);
+
+        htmlOutputFrame.srcdoc = templateText;
+        htmlOutputFrame.hidden = false;
+        htmlOutputHeader.hidden = false;
+        downloadButton.hidden = false;
+    }
+    catch (error) 
+    {
+        console.error(error);
+    }
+});
+
 
 const templateFilePrefix: string = "index-5-29";
 
@@ -81,3 +113,14 @@ async function checkDownloadStore(): Promise<string> {
 
     return returnString;
 }
+
+function handleDownload() {
+    const htmlText: string = htmlOutputFrame.srcdoc;
+    const element: HTMLAnchorElement = document.createElement('a');
+    const file = new Blob([htmlText], {type: 'text/html'});
+    element.href = URL.createObjectURL(file);
+    element.download = "swagger_ui.html";
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
+  };
